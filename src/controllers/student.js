f53eb43ce182efa1ba781d43559c49ce4b72af0a
ShahebazKhanPathan <pathanshahebazk@gmail.com>
2024-08-15 13:05:55 +0530

    changes

--- src/controllers/student.js
+++ src/controllers/student.js
@@ -1,28 +1,57 @@
 const Joi = require("joi");
-const Student = require("../models/student");
+const { Student, Details } = require("../models/student");
 
 // Controller to add new student
 const addStudent = async (req, res, next) => {
-    const { name, subject, marks } = req.body;
+    try {
+        const { name, subject, marks } = req.body;
+    
+        // Validate student
+        const { error } = validateStudent(name, subject, marks);
+        if (error) return res.status(400).send(error.message);
+    
+        // Check if student already exists
+        const isExist = await Student.findOne({ name: name });
+        if (isExist) {
+            const details = await Details.findOne({ subject: subject });
+            if (details) {
+                // Update details
+                const updateDetails = await Details.findOneAndUpdate({ subject: subject }, { marks: marks });
+                if (updateDetails) return res.send("Details updated successfully.");
+            }
+            else {
+                // Add new details
+                const studentDetails = new Details({
+                    sId: isExist._id,
+                    subject: subject,
+                    marks: marks
+                });
 
-    // Validate student
-    const { error } = validateStudent(name, subject, marks);
-    if (error) return res.status(400).send(error.message);
-
-    // Check if student already exists
-    const isExist = await Student.findOne({ name: name, subject: subject });
-    if (isExist) return res.status(409).send("Student already exists.");
-
-    // Create student instance
-    const student = new Student({
-        name: name,
-        subject: subject,
-        marks: marks
-    });
-
-    // Save student
-    const result = await student.save();
-    if (result) return res.status(200).send("Student added successfully!");
+                const saveDetails = await studentDetails.save();
+                if (saveDetails) return res.send("Details added successfully.");
+            }
+        }
+        else {
+            // Create student instance
+            const student = new Student({
+                name: name
+            });
+        
+            // Create details instance 
+            const studentDetails = new Details({
+                subject: subject,
+                marks: marks
+            });
+        
+            // Save student and details
+            const studentSaved = await student.save();
+            const detailsSaved = await studentDetails.save();
+            if (studentSaved && detailsSaved) return res.status(200).send("New student added successfully!");
+        }
+    }
+    catch (err) {
+        return res.status(500).send(err.message);
+    }
 }
 
 // Controller to fetch all students
@@ -32,6 +61,7 @@ const getStudents = async (req, res, next) => {
     res.send(students);
 }
 
+// Controller to update student
 const updateStudent = async (req, res, next) => {
     const { name, subject, marks, id } = req.body;
     const update = await Student.findByIdAndUpdate(id, { name: name, subject: subject, marks: marks });
@@ -57,7 +87,7 @@ function validateStudent(name, subject, marks) {
         marks: Joi.number().required()
     });
 
-    return schema.validate({ name, subject, marks });
+    return schema.validate({ name: name, subject: subject, marks: marks });
 }
 
 module.exports = { addStudent, getStudents, deleteStudent, updateStudent };
--- src/models/student.js
+++ src/models/student.js
@@ -6,7 +6,11 @@ const studentSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 3,
-    },
+    }
+});
+
+// Student details schema
+const studentDetails = new mongoose.Schema({
     subject: {
         type: String,
         required: true,
@@ -17,9 +21,11 @@ const studentSchema = new mongoose.Schema({
         required: true,
         min: 0,
     },
+    sId: mongoose.Schema.ObjectId
 });
 
 // Create student model
 const Student = mongoose.model("Student", studentSchema);
+const Details = mongoose.model("Details", studentDetails);
 
-module.exports = Student;
+module.exports = { Student, Details };
